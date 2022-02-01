59a4e7fce223a847c4308f38629d14e77a19a8e3
Lastzlo <z.last.zlo@gmail.com>
2022-02-01 20:38:33 +0200

    init commit

---
+++ src/main/java/com/example/govhalper/GovHalperApplication.java
@@ -0,0 +1,13 @@
+package com.example.govhalper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovHalperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovHalperApplication.class, args);
+	}
+
+}
