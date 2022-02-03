2af4e9319f03b0cfc29cfa85b27823d9e21b2370
Lastzlo <z.last.zlo@gmail.com>
2022-02-04 01:44:30 +0200

    refactor (DocumentService) add SheetGovernmentCaseAdapter to constructor, add method synchronization

--- src/main/java/com/example/reyestrHelper/servises/DocumentService.java
+++ src/main/java/com/example/reyestrHelper/servises/DocumentService.java
@@ -1,44 +1,109 @@
 package com.example.reyestrHelper.servises;
 
+import com.example.reyestrHelper.entity.SheetGovernmentCase;
+import com.example.reyestrHelper.entity.SheetGovernmentCaseStatus;
+import com.example.reyestrHelper.repositories.GoogleSheetRepository;
 import com.example.reyestrHelper.repositories.ReyestrGovUaRepository;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Log4j2
 @Service
 public class DocumentService {
 
 	private ReyestrGovUaRepository reyestrRepo;
-
-//	private GoogleSheetRepository googleSheetRepository;
+	private SheetGovernmentCaseAdapter caseAdapter;
 	private HtmlParser htmlParser;
 
+	@Value("${googleSheets.tokensDirectoryPath}")
+	private String tokensDirectoryPath;
+
+	@Value("${googleSheets.applicationName}")
+	private String applicationName;
+
+
+
 	@Autowired
-	public DocumentService(ReyestrGovUaRepository reyestrRepo, HtmlParser htmlParser) {
+	public DocumentService(
+			ReyestrGovUaRepository reyestrRepo,
+			SheetGovernmentCaseAdapter caseAdapter,
+			HtmlParser htmlParser
+	) {
 		this.reyestrRepo = reyestrRepo;
 		this.htmlParser = htmlParser;
+		this.caseAdapter = caseAdapter;
 	}
 
-//	@SneakyThrows
-//	public void synchronization() {
-//		googleSheetRepository = GoogleSheetRepository
-//				.create(GoogleConventionalFactory.getCredentials());
-//		// Get Connection
-//		// start sync
-//		//  read sheet and start iteration
-//		// for(row){
-//		// handleRow(row)
-//		// }
-//	}
+	@SneakyThrows
+	public void synchronization(
+			String credentialsPass,
+			String documentKey,
+			String range
+	) {
+		// get Connection
+		GoogleCredentialsFactory credentialsFactory = new GoogleCredentialsFactory(credentialsPass, tokensDirectoryPath);
+		GoogleSheetRepository googleSheetRepository = GoogleSheetRepository.create(credentialsFactory.getCredentials(), documentKey, range, applicationName);
 
-	private void handleRow(Object row) {
-//		long lastCount = getCountOfDocuments()
+		// start sync
+		List<List<Object>> values = googleSheetRepository.getValues();
+
+		//  read sheet and start iteration
+		List<SheetGovernmentCase> cases = caseAdapter.valuesToSheetGovernmentCases(values, range);
+		for (SheetGovernmentCase sheetCase : cases) {
+			List<List<Object>> lists = handleCase(sheetCase);
+			googleSheetRepository.updateRow(lists, sheetCase.getRange());
+		}
+		log.info("synchronization finished");
 	}
 
-	public long getCountOfDocuments(String caseNumber) {
-		String html = reyestrRepo.loadPageByCaseNumber(caseNumber);    // load html
+	private List<List<Object>> handleCase(SheetGovernmentCase sheetCase) {
+		log.info(String.format("case number %s is processing ", sheetCase.getCaseNumber()));
+		if (sheetCase.getStatus().equals(SheetGovernmentCaseStatus.NEW)) {
+			return handleNewCase(sheetCase);
+		} else {
+			return handleTraceableCase(sheetCase);
+		}
+	}
 
+	private List<List<Object>> handleTraceableCase(SheetGovernmentCase sheetCase) {
+		int count = getCountOfDocuments(sheetCase.getCaseNumber());
+
+		LocalDateTime dateTime = LocalDateTime.now();
+
+		sheetCase.setPrevUpdateTime(sheetCase.getLastUpdateTime());
+		sheetCase.setPrevUpdateCount(sheetCase.getLastUpdateCount());
+		sheetCase.setLastUpdateTime(dateTime);
+		sheetCase.setLastUpdateCount(count);
+		sheetCase.setDifference(count - sheetCase.getPrevUpdateCount());
+
+		return caseAdapter.sheetGovernmentCasesToValues(sheetCase);
+	}
+
+	private List<List<Object>> handleNewCase(SheetGovernmentCase sheetCase) {
+		int count = getCountOfDocuments(sheetCase.getCaseNumber());
+
+		LocalDateTime dateTime = LocalDateTime.now();
+
+		sheetCase.setPrevUpdateTime(dateTime);
+		sheetCase.setPrevUpdateCount(count);
+		sheetCase.setLastUpdateTime(dateTime);
+		sheetCase.setLastUpdateCount(count);
+		sheetCase.setDifference(0);
+
+		return caseAdapter.sheetGovernmentCasesToValues(sheetCase);
+	}
+
+	public int getCountOfDocuments(String caseNumber) {
+		String html = reyestrRepo.loadPageByCaseNumber(caseNumber);
+		log.info("html page by case number was loaded");
 		return htmlParser
-				.getCountOfDocumentsByCaseNumber(html, caseNumber);    // parse html and return count of documents
+				.getCountOfDocumentsByCaseNumber(html, caseNumber);
 	}
 
 }
